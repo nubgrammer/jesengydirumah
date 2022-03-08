a4fb2a3c366b104c4d14edaf7089e0b1b1d38497
nubgrammer <bambanglheon@gmail.com>
2022-03-08 15:28:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var tanya = prompt("After u see this website u promise we are still best friennd? Answer with Yes / No");
+
+if (tanya == "Yes") {
+  alert("ok");
+} else {
+  window.location.href = "www.google.com";
+}
